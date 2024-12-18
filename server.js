93f34e9a9254419dfad5e69d6b0c1161e0844851
Yaaj2002 <84038875+Yaaj2002@users.noreply.github.com>
2024-12-18 14:34:10 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDZ6NmEP_B9TLrE44pL-T5wz7SSMVT8SU",
+    authDomain: "test-project-52cda.firebaseapp.com",
+    projectId: "test-project-52cda",
+    storageBucket: "test-project-52cda.firebasestorage.app",
+    messagingSenderId: "995224692112",
+    appId: "1:995224692112:web:de85f3c6ada9b3c1c6bf15"
+  };
